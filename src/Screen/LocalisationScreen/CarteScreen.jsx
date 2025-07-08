dcb98dce7121c24bf87f9b7f7a95fc8fa5205545
Avotriniaina <avotriniainajonatan@gmail.com>
2025-07-08 17:44:50 +0200

    screen

---
+++ src/Screen/LocalisationScreen/CarteScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const CarteScreen= ()=>{
+
+    return(
+        <View>
+            <Text>Localisation chantier</Text>
+        </View>
+    )
+}
+
+export default CarteScreen;
---
+++ src/Screen/NewChantierScreen/NewScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const NewScreen= ()=>{
+
+    return(
+        <View>
+            <Text>Nouveau chantier</Text>
+        </View>
+    )
+}
+
+export default NewScreen;
