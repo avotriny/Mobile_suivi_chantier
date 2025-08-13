ec2d431b88d7f14a682ca6b42e6df3500f266139
Avotriniaina <avotriniainajonatan@gmail.com>
2025-08-13 08:03:49 +0200

    Step sur ajout de chantier

--- src/Screen/NewChantierScreen/NewScreen.jsx
+++ src/Screen/NewChantierScreen/NewScreen.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import DetailsScreen from './DetailsScreen/DetailScreen';
+import LocationScreen from './LocationScreen/LocationScreen';
+import ImagesScreen from './ImagesScreen/ImagesScreen';
 
-const NewScreen= ()=>{
 
-    return(
-        <View>
-            <Text>Nouveau chantier</Text>
-        </View>
-    )
+const Stack = createStackNavigator();
+export default function ChantierNavigator() {
+  return (
+    <Stack.Navigator initialRouteName="Details">
+      <Stack.Screen name="Details" component={DetailsScreen} />
+      <Stack.Screen name="Location" component={LocationScreen} />
+      <Stack.Screen name="Images" component={ImagesScreen} />
+    </Stack.Navigator>
+  );
 }
-
-export default NewScreen;
